689b779b4b78cfadd99aaa08291dd780d4c7db56
Marley Castro <162909728+MarleyCastro@users.noreply.github.com>
2025-06-12 18:59:54 -0300

    Add files via upload

---
+++ caracterstica/People.java
@@ -0,0 +1,13 @@
+package caracterstica;
+
+public class People {
+    public String sexo;
+    public int idade;
+    public int Corcabelo;
+
+    void masculino(){
+        if(sexo == "m"){
+            System.out.println("Tem " );
+        }
+    }
+}
